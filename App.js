8bfc5f6d2f0e153d87c2c148ebad0798b04abbd9
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2020-02-11 13:44:30 -0700

    Basic Restructuring, Part 1
    -Basic page rendering has been reconstructed, placing the necessary information in the Maps.js file.
    -The stylesheet defining the look of the page will soon be modified with some booleans for flags.
    -App.js has been significantly reduced in its size, mostly due to shifting data to the Maps.js file.
    -GamestateRedstoneUpdate.js has had some significant rework, with much more code added to the basic game engine in preparation for the implementation of flags.
    -Further pages have been added to the Maps.js file, bringing the page count up from 3 pages (or 1 page that can actually be rendered) to 5 pages.
    -Herobrine has been removed.

--- App.js
+++ App.js
@@ -1,46 +1,138 @@
-import React from 'react';
-import logo from './logo.svg';
-import ReactDOM from 'react-dom'
+import React, { useState } from 'react';
+import {GameState} from './GamestateRedstoneUpdate.js';
+import './stylesheet.css';
+import {mapData} from './Maps.js'
 
-const divstyle = {
-  margin: "1px",
-  height: "15px",
-  width: "15px",
-  color: "white",
-  borderColor: "black",
-  borderStyle: "solid",
+let nextMap = {
+  map1: "map2",
+  map2: "map3",
+  map3: "map4",
+  map4: "map5",
 }
 
-class Map extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Tile>F</Tile>
-        <Tile />
-        <Tile />
-      </React.Fragment>
-      )
-  }
-};
 
-class Tile extends React.Component {
-  render() {
-    return <div className = "tile" style = {divstyle}>{this.props.children}</div>;
-  }
-};
-
-
+let data = {
+}
 
 function App() {
+  // logic to set data to current map
+  const [currentMap, setCurrentMap] = useState("map1")
+  const [chosenOption, setChosenOption] = useState("")
+  const [GS, setGS] = useState(new GameState)
+
+  React.useEffect(() => {
+    GS.setFlag("Started")
+    console.log(GS.isSet("Started"))
+  }, [])
+
+  // set data to the value of storage[map]
+
+  let data = mapData[currentMap]
+
+  function NextPage() {
+    setCurrentMap(nextMap[currentMap])
+  }
+
   return (
-    <div className="App">
-      Hello, folks
-      <Map>
-      </Map>
+  <div className="App">
+    
+    <h2 id="encounter">Chapter 1: Close Encouter of the Moose Kind</h2>
+    
+    <div id="statusBar">
+      <DisplayStatusBar statusBar = {data.status} />
     </div>
+    
+    <div id="inventory">
+      <DisplayInventory inventory = {data.inventory} />
+    </div>
+    
+    <div id="body">
+      <DisplayDescription description = {data.main} />
+      <DisplayDecisions options = {data.choices} changeMapFn={NextPage}/>  
+    </div> 
+  
+  </div>
   );
 }
 
-export default App;
+function DisplayDescription (props) {
+  return props.description.map(quote => <p>{quote}</p>)
+}
 
-ReactDOM.render(<App />, document.getElementById('root'))
+function DisplaySingleOption (props) {
+ const [isRevealed, setIsRevealed] = React.useState(false)
+
+return (
+      <React.Fragment>
+        <button style = {{visibility: props.isAvailable ? "visible" : "hidden"}} onClick={() => {
+          props.setIsRevealed(true);
+          props.setter(false)
+        }}><h5>{props.option.buttonText}</h5></button>
+        <p style = {{visibility: props.isRevealed ? "visible" : "hidden"}}>{props.option.quote}</p>
+      </React.Fragment>
+    )
+}
+
+function DisplayDecisions (props) {
+ const [showContinueButton, setShowContinueButton] = React.useState(true)
+ const [isRevealed, setIsRevealed] = React.useState(new Array(props.options.length).fill(false))
+ function createSetSingleOption(index) {
+  return function (bool) {
+    // use setHasBeenRevelead to set the bool at index given.
+    let newValues = isRevealed
+    newValues[index] = bool
+    console.log(`Setting has been revelead. ${JSON.stringify(newValues)}`)
+    setIsRevealed(newValues)
+  }
+}
+
+  const output = props.options.map((option, index) => <DisplaySingleOption isRevealed={isRevealed[index]} setIsRevealed={createSetSingleOption(index)} isAvailable = {showContinueButton} setter = {setShowContinueButton} option = {option}/>)
+
+  return (
+    <div>
+      {output}
+      <button style = {{visibility: showContinueButton ? "hidden" : "visible"}} onClick={() => {
+        setIsRevealed(Array(props.options.length).fill(false));
+        setShowContinueButton(true)
+        props.changeMapFn()
+        // props.setter(false);
+      }}><h4>Click here to continue</h4>
+      </button>
+    </div>)
+}
+
+
+// props = {
+//   statusBar: {
+//      hungerLevel: "Sated",
+//      thirstLevel: "Parched",
+//      warmthLevel: "Cold as hell"
+//    },
+//    inventory: ["Wallet", "Watch"]
+//}
+
+function DisplayStatusBar (props) {
+  return(
+    <React.Fragment>
+    <h3>Status</h3>
+    <ul>
+    {props.statusBar.map(status => <li>{status}</li>)}
+    </ul>
+    </React.Fragment>
+    )
+}
+
+function DisplayInventory (props) {
+  return(
+    <React.Fragment>
+    <h3>Backpack: </h3> 
+    <ul>
+      {props.inventory.map(inventory => <li>{inventory}</li>)}
+    </ul>
+    </React.Fragment>
+  ) 
+}
+
+
+
+export default App;
